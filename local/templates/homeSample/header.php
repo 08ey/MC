1d29ab307808ae769c82e03d3ff77d518366029a
08ey <ivanpritmov@mail.ru>
2024-04-03 21:31:05 +0300

    task 162930

--- local/templates/homeSample/header.php
+++ local/templates/homeSample/header.php
@@ -1,18 +1,6 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<?
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?> <?
 IncludeTemplateLangFile(__FILE__);
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title><?$APPLICATION->ShowTitle()?></title>
-  <?$APPLICATION->ShowHead();?>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500">
-      <?
+?> <?$APPLICATION->ShowHead();?> <?
  use Bitrix\Main\Page\Asset; 
 Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/fonts/icomoon/style.css'); 
 Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/bootstrap.min.css'); 
@@ -28,29 +16,28 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/fl-bigmug-line.css');
 Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/aos.css'); 
 Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/style.css'); 
 ?>
-</head>
-
-<body>
-<div id="panel"><?$APPLICATION->ShowPanel();?></div>
-  <div class="site-loader"></div>
-
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div> <!-- .site-mobile-menu -->
-
-    <div class="border-bottom bg-white top-bar">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-6 col-md-6">
-            <p class="mb-0">
-              <?$APPLICATION->IncludeComponent(
+<div id="panel">
+	 <?$APPLICATION->ShowPanel();?>
+</div>
+<div class="site-loader">
+</div>
+<div class="site-wrap">
+	<div class="site-mobile-menu">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close mt-3">
+ <span class="icon-close2 js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body">
+		</div>
+	</div>
+	 <!-- .site-mobile-menu -->
+	<div class="border-bottom bg-white top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-6 col-md-6">
+					<p class="mb-0">
+						 <?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
 	Array(
@@ -59,20 +46,11 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/style.css');
 		"EDIT_TEMPLATE" => "",
 		"PATH" => "/local/templates/homeSample/include/phone.php"
 	)
-);$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_SHOW" => "file",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => "",
-		"PATH" => "/local/templates/homeSample/include/mail.php"
-	)
 );?>
-            </p>
-          </div>
-          <div class="col-6 col-md-6 text-right">
-            <?$APPLICATION->IncludeComponent(
+					</p>
+				</div>
+				<div class="col-6 col-md-6 text-right">
+					 <?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
 	Array(
@@ -82,16 +60,15 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/style.css');
 		"PATH" => "/local/templates/homeSample/include/icons.php"
 	)
 );?>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    <div class="site-navbar">
-      <div class="container py-1">
-        <div class="row align-items-center">
-          <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><?$APPLICATION->IncludeComponent(
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="site-navbar">
+		<div class="container py-1">
+			<div class="row align-items-center">
+				<div class="col-8 col-md-8 col-lg-4">
+					<h1><?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
 	Array(
@@ -101,42 +78,55 @@ Asset::getInstance()->addCss(SITE_TEMPLATE_PATH .'/css/style.css');
 		"PATH" => "/local/templates/homeSample/include/logo.php"
 	)
 );?></h1>
-          </div>
-          <div class="col-4 col-md-4 col-lg-8">
-            <nav class="site-navigation text-right text-md-right" role="navigation">
-
-              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
-                  class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
-                                <li class="active">
-                  <a href="index.html">Home</a>
-                </li>
-                <li class="has-children">
-                  <a href="properties.html">Properties</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Buy</a></li>
-                    <li><a href="#">Rent</a></li>
-                    <li><a href="#">Lease</a></li>
-                    <li class="has-children">
-                      <a href="#">Menu</a>
-                      <ul class="dropdown">
-                        <li><a href="#">Menu One</a></li>
-                        <li><a href="#">Menu Two</a></li>
-                        <li><a href="#">Menu Three</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-              </ul>
-            </nav>
-          </div>
-
-
-        </div>
-      </div>
-    </div>
-  </div>
+				</div>
+				<div class="col-4 col-md-4 col-lg-8">
+ <nav class="site-navigation text-right text-md-right" role="navigation">
+					<div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
+ <a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a>
+					</div>
+					 <?$APPLICATION->IncludeComponent(
+	"bitrix:menu", 
+	".default", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "Y",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
+					<ul class="site-menu js-clone-nav d-none d-lg-block">
+						<li class="active"> <a href="index.html">Home</a> </li>
+						<li class="has-children"> <a href="properties.html">Properties</a>
+						<ul class="dropdown">
+							<li><a href="#">Buy</a></li>
+							<li><a href="#">Rent</a></li>
+							<li><a href="#">Lease</a></li>
+							<li class="has-children"> <a href="#">Menu</a>
+							<ul class="dropdown">
+								<li><a href="#">Menu One</a></li>
+								<li><a href="#">Menu Two</a></li>
+								<li><a href="#">Menu Three</a></li>
+							</ul>
+ </li>
+						</ul>
+ </li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+ </nav>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+ <br>
