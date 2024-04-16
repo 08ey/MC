1a4a8aeb02b286decb43c9c115a1b9a5e1e1c92b
08ey <ivanpritmov@mail.ru>
2024-04-16 14:19:25 +0300

    test1

--- test.php
+++ test.php
@@ -5,20 +5,16 @@ $APPLICATION->SetTitle("Новая страница");
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-lg-8 mb-5">
-				<?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form", 
-	"auth", 
-	array(
-		"FORGOT_PASSWORD_URL" => "/user/",
-		"PROFILE_URL" => "",
-		"REGISTER_URL" => "/user/register.php",
-		"SHOW_ERRORS" => "N",
-		"COMPONENT_TEMPLATE" => "auth"
-	),
-	false
-);?>
 			</div>
 		</div>
 	</div>
 </div>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?$APPLICATION->IncludeComponent("bitrix:main.feedback", "template1", Array(
+	"EMAIL_TO" => "ivanpritmov@mail.ru",	// E-mail, на который будет отправлено письмо
+		"EVENT_MESSAGE_ID" => "",	// Почтовые шаблоны для отправки письма
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",	// Сообщение, выводимое пользователю после отправки
+		"REQUIRED_FIELDS" => "",	// Обязательные поля для заполнения
+		"USE_CAPTCHA" => "Y",	// Использовать защиту от автоматических сообщений (CAPTCHA) для неавторизованных пользователей
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
